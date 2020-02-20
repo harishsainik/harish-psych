64af105cc8299ba3b8eca0d8b338d3075d69ea84
harish saini <harish.saini@moglix.com>
2020-02-20 08:07:35 +0530

    Initial commit with basic setup done

---
+++ src/main/java/com/psych/harishpsych/HarishPsychApplication.java
@@ -0,0 +1,13 @@
+package com.psych.harishpsych;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarishPsychApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarishPsychApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/psych/harishpsych/HarishPsychApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psych.harishpsych;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarishPsychApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
